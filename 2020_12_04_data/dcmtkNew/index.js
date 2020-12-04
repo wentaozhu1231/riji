34645fb488d4e68739820a996f0e009843bd6a8c
wentaozhu1231 <zwtgy918@gmail.com>
2020-12-04 17:56:51 +0800

    working dairy

---
+++ 2020_12_04_data/dcmtkNew/index.js
@@ -0,0 +1,8 @@
+const testAddon = require('./testaddon.node');
+
+
+
+const value = "F:\\QC_data\\RI.1.3.46.423632.131000.1606839311.17.dcm";
+console.log('symmetry of this graph is:',testAddon.print(value));
+
+module.exports = testAddon;
